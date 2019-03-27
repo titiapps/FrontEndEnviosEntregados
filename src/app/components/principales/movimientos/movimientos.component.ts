4da84bccd2175e8c0d202c1018f50d9976fe8951
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-03-27 17:55:38 -0600

    Cambios del dia

--- src/app/components/principales/movimientos/movimientos.component.ts
+++ src/app/components/principales/movimientos/movimientos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { MovimientosService } from "src/app/services/services.index";
+import { HttpClient } from "@angular/common/http";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-movimientos",
@@ -9,21 +12,42 @@ import { MovimientosService } from "src/app/services/services.index";
 })
 export class MovimientosComponent implements OnInit {
   id_movimiento: String;
+  etiqueta: String;
+
   constructor(
     private _router: Router,
     private _activatedRoute: ActivatedRoute,
-    private _movimientosService: MovimientosService
+    private _movimientosService: MovimientosService,
+    private _http: HttpClient,
+    private domSanitizer: DomSanitizer
   ) {
     this._activatedRoute.params.subscribe(params => {
       this.id_movimiento = params.id;
     });
   }
 
-  ngOnInit() {
-    this._movimientosService
-      .regresarInformacionMovimiento(this.id_movimiento)
-      .subscribe(resp => {
-        console.log(resp);
-      });
+  async ngOnInit() {
+    let respuesta: any = await this.regresarInformacionMovimiento(
+      this.id_movimiento
+    );
+
+    this.etiqueta = respuesta.res_movimiento.etiqueta;
+    console.log(this.etiqueta);
+  }
+
+  //esta nos regresa la informacion completa ocn el movimiento que se hizo para generar la etiqueta
+  regresarInformacionMovimiento(id_movimiento) {
+    return new Promise((resolve, reject) => {
+      this._movimientosService
+        .regresarInformacionMovimiento(id_movimiento)
+        .subscribe(
+          (resp: any) => {
+            resolve(resp);
+          },
+          err => {
+            reject(err);
+          }
+        );
+    });
   }
 }
